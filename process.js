1f34b061cbeffa3505918b68ebaae7b737cf5295
wybrand <wybrandvdmeer@gmail.com>
2025-07-09 09:54:28 +0200

    Can now detect how long the computer was suspended (lid closed in case of laptop).

--- process.js
+++ process.js
@@ -1,5 +1,5 @@
 import {log, loggerEnabled, enableLogging} from './log.js'
-import {IDLE_DETECTION_INTERVAL, start, started} from './globals.js'
+import {IDLE_DETECTION_INTERVAL, HEARTBEAT_INTERVAL, start, started} from './globals.js'
 
 var called = 0
 
@@ -56,12 +56,16 @@ async function process_impl() {
             }
         }
     }
-    
+
     var groups2open = hosts2groups(groups, hosts2open)
+    var suspensionTime = 0
+    if(groups2open.length > 0) {
+        suspensionTime = await detectSuspensionTime()
+    }
 
     var saveOpen=false
     groups2open.forEach(g => {
-        if(open(g)) {
+        if(open(g, suspensionTime)) {
             saveOpen = true
         }
     })
@@ -79,8 +83,31 @@ async function process_impl() {
     if(saveStarted || saveOpen || saveClose) {
         await chrome.storage.local.set({'groups': groups})
     }
+    
+    await chrome.storage.local.set({'heartBeat': getCurrentTime()})
 
     redirectTabs(groups, tabs)
+}
+
+/*
+Sometimes the closing of the lid event is not detected. Therefore this mechanism 
+is devised to detect how long the lid was closed (and computer suspended).
+When detected the remainder of the group is corrected.
+*/
+async function detectSuspensionTime() {
+    var suspensionTime=0
+    var heartBeat = await chrome.storage.local.get('heartBeat')
+    if(heartBeat != null) {
+        heartBeat = heartBeat.heartBeat
+        var currentTime = getCurrentTime()
+        if(currentTime - heartBeat > HEARTBEAT_INTERVAL + 100) {
+            suspensionTime = (currentTime - heartBeat)/1000
+        }
+    }
+
+    log('Suspension-time: ' + suspensionTime)
+
+    return suspensionTime
 }
 
 async function redirectTabs(groups, tabs) {
@@ -187,24 +214,26 @@ export function close(group) {
         return false
     }
 
-    var currentTime = getCurrentTime();
-    group.remaining = group.remaining - Math.floor((currentTime - group.start)/1000);
+    var currentTime = getCurrentTime()
+    group.remaining = group.remaining - Math.floor((currentTime - group.start)/1000)
 
     if(group.remaining < 0) {
-        group.remaining = 0;
+        group.remaining = 0
     }
 
-    log('close(' + group.name + ', remaining: ' + group.remaining + ')');
+    log('close(' + group.name + ', remaining: ' + group.remaining + ')')
 
-    group.start = null;
+    group.start = null
 
     return true
 }
 
-export function open(group) {
+export async function open(group, suspensionTime) {
     if(!inActiveInterval(group)) {
         return false
     }
+
+    log('START: ' + group.start + ', sus: ' + suspensionTime)
     
     var currentTime = getCurrentTime();
     var open=false
@@ -213,10 +242,14 @@ export function open(group) {
         group.start = currentTime;
         group.remaining = group.duration * 60;
         open = true
-    }  else if(group.start == null) {
+    } else if(group.start == null) {
         group.start = currentTime;
         open = true
-    } 
+    } else if(suspensionTime > 0) { // Already opened.
+        log('Detected suspensionTime, correct remaining of group -> ' + group.name + ': ' + group.remaining + ' -> ' + (group.remaining + suspensionTime))
+        group.remaining += suspensionTime
+        open = true
+    }
 
     group.lastUpdated = currentTime
           
